1edcd1078bb1986a12810f8e0a1ec91193573fcb
DanielLopez <daniel.lopezing20@gmail.com>
2025-03-05 20:33:33 -0600

    Add logic to read input data

--- capital_gains/main.py
+++ capital_gains/main.py
@@ -1,6 +1,20 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import sys
+import json
+
+
+def main():
+    print("Inicio del sistema")
+    input_data = sys.stdin.read()
+
+    try:
+        operations = json.loads(input_data)
+        for operation in operations:
+            print(f"Operación: {operation['operation']}, "
+                  f"Precio unitario: {operation['unit-cost']}, "
+                  f"Cantidad: {operation['quantity']}")
+    except json.JSONDecodeError:
+        print("Error al parsear información de entrada")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
